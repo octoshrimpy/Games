56498aba4a39df1d9e7e5cd78dfe8a0eac16f287
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-01-02 22:50:59 -0700

    Made RPS better.

--- RPS.rb
+++ RPS.rb
@@ -11,25 +11,24 @@ def rst_scores()
 		compatt = 0
 		playeratt = 0
 
-		puts "\n\nType your selection to play. Type N to quit or Reset to reset scores."
-		playeratt = gets.chomp.downcase
-		if ["r", "rock"].include?(playeratt)
-			playeratt = 1
-		elsif [ "p", "paper"].include?(playeratt)
-			playeratt = 2
-		elsif ["s", "scissors"].include?(playeratt)
-			playeratt = 3
-		elsif ["n", "no"].include?(playeratt)
-			i = 1
-		elsif ["reset"].include?(playeratt)
+		puts "Type your selection to play. Type N to quit or Reset to reset scores."
+		playeratt = gets.chomp.downcase.split("")
+		system "clear" or system "cls"
+		rock = (playeratt & "rock".split("")).length
+		paper = (playeratt & "paer".split("")).length
+		scissors = (playeratt & "scior".split("")).length
+		break if playeratt.join == "n" || playeratt.join == "no" || playeratt.join == "x"
+		if playeratt.join == "reset"
 			rst_scores()
-		else puts "\n\nInvalid Statement"
+		elsif rock >= paper && rock >= scissors
+			playeratt = 1
+		elsif paper >= scissors
+			playeratt = 2
+		else
+			playeratt = 3
 		end
 
-		break if i == 1
-
 		compatt = rand(1..3)
-		puts "\n\n"
 		if compatt == playeratt
 			puts  "      It's a draw!"
 			dro += 1
