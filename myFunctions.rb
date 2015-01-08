267cdb0f98484aa28cb0aa6e38403d521be78116
Rocco Nicholls <rocco11nicholls@gmail.com>
2015-01-07 22:05:17 -0700

    Created the pixelart converter tool that takes pixel art and converts it into the formulas used by pet.rb to draw, then displays a sample of what the art will look like.

--- myFunctions.rb
+++ myFunctions.rb
@@ -1,4 +1,5 @@
-@hold = []
+@empty = ". "
+@mark = "• "
 input = "
 123, -46, 0, -1, 1, 52, 1113245
 [8].each { |y| [0, 1].each { |x| coords << [y, x] }}
@@ -7,19 +8,30 @@ input = "
 [1, 6].each { |y| [-2, 3].each { |x| coords << [y, x] }}
 [0].each { |y| (-1..2).each { |x| coords << [y, x] }}
 "
-increment = 1
 
-def incrementAll(input, increment)
+art = "
+   •••
+  •   •
+ • • • •
+•  • •  •
+•       •
+•       •
+ •••••••
+"
+
+def incrementAll(string, increment)
+  hold = []
   puts string
   puts ""
-  string.split("").each do |char|
+  array = string.split("")
+  array.each do |char|
     if (char =~ /\A[-+]?\d+\z/) == 0 || char == "-"
-      @hold << char
+      hold << char
     else
-      if @hold.length > 0
-        num = @hold.join.to_i
+      if hold.length > 0
+        num = hold.join.to_i
         num += 1
-        @hold = []
+        hold = []
         print num
       end
       num = char
@@ -28,8 +40,51 @@ def incrementAll(input, increment)
   end
 end
 
-def pixelArt2Function(pixel_art)
+def pixelartToFunction(pixel_art, live="x", dead=" ")
+  lines = pixel_art.split("\n")
+  lines -= [""]
+  placements = Array.new(lines.length) { [] }
+  lines.reverse.each_with_index do |line, line_num|
+    line.split("").each_with_index do |char, char_pos|
+      placements[line_num] << char_pos if char == live
+    end
+  end
+  min = placements.sort_by { |y_values| y_values.first }.first.first
+  max = placements.sort_by { |y_values| y_values.last }.last.last
+  width = max - min
+  center = (width.to_f / 2).round
+  @boardy = placements.length
+  @boardx = max + 1
+  @board = Array.new(@boardy) {Array.new(@boardx) {@empty}}
+  draw = []
+  placements.each_with_index do |coords, index|
+    x_values = []
+    coords.each do |x|
+      x_values << x - center
+      draw << [index + 1, x]
+    end
+    puts "[#{index}].each { |y| [#{x_values.join(", ")}].each { |x| coords << [y, x] }}"
+  end
+
+  draw.each do |coord|
+    @board[-coord[0]][coord[1]] = @mark
+  end
+  i = 0
+  print "  "
+  @boardx.times do |x|
+    print "#{x} " if x < 10
+    print "#{x}" if x >= 10
+  end
+  puts ""
+  while i < @board.length
+    print "#{i} " if i < 10
+    print "#{i}" if i >= 10
+    puts @board[i].join
+    i += 1
+  end
 end
+
+pixelartToFunction(art, "•", " ")
 
 
 # 123, -46, 0, -1, 1, 52, 1113245
